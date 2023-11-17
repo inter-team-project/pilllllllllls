df1280ae3a1d6fa42204be90583e384d92805c60
miiiiiiiiiin <149678027+miiiiiiiiiin@users.noreply.github.com>
2023-11-17 20:31:49 +0900

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Application12113333333333"
+include(":app")
+ 
